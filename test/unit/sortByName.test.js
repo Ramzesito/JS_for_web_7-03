2c62b9b9edc27916c55699ef4c48c0cc334d2781
Ramzesito <roman_shv@mail.ru>
2023-11-13 15:53:20 +0300

    completed 1 task

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -2,16 +2,31 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+
+    const testCase1 = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const testCase2 = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Властелин Колец",
+    ];
+
+    const expected1 = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const expected2 = [
+      "Властелин Колец",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ];
+
+    expect(sorting.sortByName(testCase1)).toEqual(expected1);
+    expect(sorting.sortByName(testCase2)).toEqual(expected2);
   });
 });
